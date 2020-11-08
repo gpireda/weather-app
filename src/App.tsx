927b50f5a20caf065acbc33eb4c42f61f725329b
Gabriel Pireda <gabriel.pireda@gmail.com>
2020-11-08 17:35:47 -0300

    implements fetch cities

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,30 @@ import React from 'react'
 import './App.css'
 
 import Map from 'components/Map/Map'
+
+import useCities from 'hooks/useCities'
 import useCoordinates from 'hooks/useCoordinates'
+import client from 'utils/client'
 
 const App = () => {
   const { isLoading, latitude, longitude, onCoordinatesChange } = useCoordinates()
+  const { onCitiesChanged, onCitiesReset } = useCities()
 
   if (isLoading) {
     return <p>Loading</p>
   }
 
+  function handleFetchCitiesButtonClick() {
+    onCitiesReset()
+
+    client.fetchCities({ latitude: latitude!, longitude: longitude! }).then(onCitiesChanged)
+  }
+
   return (
     <>
       <Map latitude={latitude!} longitude={longitude!} onCoordinatesChange={onCoordinatesChange} />
+
+      <button onClick={handleFetchCitiesButtonClick}>Search</button>
     </>
   )
 }
